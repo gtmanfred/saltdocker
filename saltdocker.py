b64f314b3152d4639ce22d087f54112014be7dfb
Daniel Wallace <danielwallace@gtmanfred.com>
2019-03-26 17:23:34 -0500

    master and api are working

--- saltdocker.py
+++ saltdocker.py
@@ -1,36 +1,77 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import datetime
 import distutils.version
+import os
 import signal
+import tempfile
 
 import aiohttp
-import aiodocker
+import jinja2
 
 MINVER = distutils.version.LooseVersion('2017.7.0')
+PATH = os.path.dirname(os.path.abspath(__file__))
+with open(f'{PATH}/Dockerfile.j2') as dockerfile:
+    DOCKERTEMPLATE = jinja2.Template(dockerfile.read())
 
 
 class SaltVersion(object):
 
     loop = asyncio.get_event_loop()
     versions = []
+    date = datetime.datetime.utcnow().strftime("%Y%M%d")
 
     def __init__(self, version):
         self.version = version
 
-    async def __call__(self):
-        print(self.version)
-        await asyncio.sleep(1)
+    async def __call__(self, force=False):
+        try:
+            tmpfile = tempfile.mkstemp()
+            with open(tmpfile[1], 'w') as dfile:
+                print(DOCKERTEMPLATE.render(salt_version=self.version), file=dfile)
+            
+            cwd = os.getcwd()
+            if cwd != PATH:
+                os.chdir(PATH)
+
+            args = ['docker', 'build', '--file', tmpfile[1]]
+
+            if force is True:
+                args.append('--no-cache')
+
+            args.extend([
+                '--tag', f'gtmanfred/saltstack:{self.version}',
+                '--tag', f'gtmanfred/saltstack:{self.version}-{self.date}',
+                PATH
+            ])
+
+            proc = await asyncio.create_subprocess_exec(*args, loop=self.loop)
+            await proc.communicate()
+        finally:
+            os.chdir(cwd)
+            os.unlink(tmpfile[1])
 
     @classmethod
-    async def get_versions(cls):
+    async def build_salt_images(cls):
         async with aiohttp.ClientSession() as session:
             async with session.get('https://pypi.org/pypi/salt/json') as response:
                 data = await response.json()
-        versions = sorted(map(distutils.version.LooseVersion, data['releases'])) 
-        for version in versions:
+        def check_version(version):
             if version < MINVER or 'rc' in version.version:
-                continue
-            cls.versions.append(cls.loop.create_task(SaltVersion(version)()))
+                return False
+            if [
+                    v for v in data['releases']
+                    if distutils.version.LooseVersion(v).version[:-1] == version.version[:-1] and
+                       distutils.version.LooseVersion(v) > version
+            ]:
+                return False
+            return True
+        versions = sorted(filter(check_version, map(distutils.version.LooseVersion, data['releases'])))
+        for idx, version in enumerate(versions):
+            if idx == 0:
+                await SaltVersion(version)(force=False)
+            else:
+                cls.versions.append(cls.loop.create_task(cls(version)()))
         await asyncio.gather(*cls.versions, loop=cls.loop)
 
 
@@ -38,9 +79,8 @@ def main():
     loop = asyncio.get_event_loop()
     for signame in {'SIGINT', 'SIGTERM'}:
         loop.add_signal_handler(getattr(signal, signame), loop.stop)
-
     try:
-        loop.run_until_complete(SaltVersion.get_versions())
+        loop.run_until_complete(SaltVersion.build_salt_images())
     finally:
         loop.close()
 
---
+++ saltinit.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import asyncio
+import json
+import os
+import signal
+
+
+async def main():
+    with open('/etc/salt/master.d/api.conf', 'w') as apifile:
+        if 'SALT_API_CONFIG' in os.environ:
+            json.dump(json.loads(os.environ['SALT_API_CONFIG']), apifile)
+        else:
+            json.dump({
+                'rest_cherrypy': {
+                    'port': 8000,
+                    'ssl_crt': '/etc/pki/tls/certs/localhost.crt',
+                    'ssl_key': '/etc/pki/tls/certs/localhost.key',
+                },
+                'external_auth': {
+                    'sharedsecret': { 
+                        'salt': ['.*', '@wheel', '@jobs', '@runner'],
+                    },
+                },
+                'sharedsecret': os.environ.get('SALT_SHARED_SECRET', 'supersecret'),
+            }, apifile)
+
+    if 'SALT_MASTER_CONFIG' in os.environ:
+        with open('/etc/salt/master.d/master.conf', 'w') as apifile:
+            json.dump(json.loads(os.environ['SALT_API_CONFIG']), apifile)
+
+    apiproc = await asyncio.create_subprocess_exec('salt-api')
+    masterproc = await asyncio.create_subprocess_exec('salt-master')
+
+    await asyncio.gather(apiproc.communicate(), masterproc.communicate())
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(getattr(signal, signame), loop.stop)
+
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
